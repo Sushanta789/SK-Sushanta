a611db95c2ae9c3ac1cb6f3d2c1c260d3a22253e
Sushanta789 <sshntkalaka@gmail.com>
2025-04-15 13:31:23 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const text = "Welcome to My Website - S.K. Sushanta";
+  let i = 0;
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("autoType").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  }
+  window.onload = typeWriter;
