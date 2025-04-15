17d453d5ba7074964713482f2ba1537921212269
Sushanta789 <sshntkalaka@gmail.com>
2025-04-15 13:43:08 +0530

    update

---
+++ Login/login.js
@@ -0,0 +1,14 @@
+function login(e) {
+    e.preventDefault();
+    const stored = JSON.parse(localStorage.getItem("userData"));
+    const inputId = document.getElementById("userId").value.trim();
+    const inputPass = document.getElementById("password").value.trim();
+
+    if (stored && stored.userId === inputId && stored.password === inputPass) {
+      localStorage.setItem("loggedIn", "true");
+      alert("Login successful!");
+      window.location.href = "home.html";
+    } else {
+      alert("Invalid credentials");
+    }
+  }
